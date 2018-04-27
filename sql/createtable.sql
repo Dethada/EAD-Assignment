53da46d305161de516fa9b60e7ac6e2e30c08f1b
Javiery3889 <javiery3889@gmail.com>
2018-04-27 10:50:00 +0800

    Move insert statements to inserttable.sql (#7)
    * Move insert statements to inserttable.sql

--- sql/createtable.sql
+++ sql/createtable.sql
@@ -1,83 +1,104 @@
 CREATE TABLE movie (
-    MovieID int NOT NULL AUTO_INCREMENT,
-    MovieTitle  varchar(200) NOT NULL,
-    ActorList   varchar(200)    NOT NULL,
-    ReleaseDate date NOT NULL,
-    Synopsis    text NOT NULL,
-    Duration    int NOT NULL,
-    PRIMARY KEY (MovieID)
+   	ID int NOT NULL AUTO_INCREMENT,
+    title  varchar(255) NOT NULL,
+    releasedate date NOT NULL,
+    synopsis    text NOT NULL,
+    duration    int NOT NULL,
+    imagepath varchar(255) NOT NULL,
+    status varchar(100) NOT NULL,
+    PRIMARY KEY (ID)
 );
 
 CREATE TABLE actor(
-    ActorID int NOT NULL AUTO_INCREMENT,
-    ActorName varchar(150) NOT NULL,
-    ActorDesc  text NOT NULL,
-    PRIMARY KEY(ActorID) 
+    ID int NOT NULL AUTO_INCREMENT,
+    Name varchar(255) NOT NULL,
+    description  text NOT NULL,
+    PRIMARY KEY(ID) 
 );
 
 CREATE TABLE MovieActor(
-    MovieID int NOT NULL AUTO_INCREMENT,
-    ActorID int NOT NULL AUTO_INCREMENT,
-    FOREIGN KEY (MovieID) REFERENCES movie(MovieID)
+    movieID int NOT NULL,
+    actorID int NOT NULL,
+    FOREIGN KEY (movieID) REFERENCES movie(ID)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-    FOREIGN KEY (ActorID) REFERENCES actor(ActorID)
+    FOREIGN KEY (actorID) REFERENCES actor(ID)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
 CREATE TABLE timeslot(
-    timing time NOT NULL,
-    MovieID int NOT NULL AUTO_INCREMENT,
+    timing datetime NOT NULL,
+    movieID int NOT NULL,
     PRIMARY KEY (timing),
-    FOREIGN KEY (MovieID) REFERENCES movie(MovieID)
+    FOREIGN KEY (movieID) REFERENCES movie(ID)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE GenreID(
-    GenreID int NOT NULL AUTO_INCREMENT,
-    GenreName varchar(150) NOT NULL,
-    GenreDesc text NOT NULL,
-    PRIMARY KEY(GenreID)
+CREATE TABLE Genre(
+    ID int NOT NULL AUTO_INCREMENT,
+    name varchar(150) NOT NULL,
+    description text NOT NULL,
+    UNIQUE (name),
+    PRIMARY KEY(ID)
 );
 
 CREATE TABLE MovieGenre(
-    MovieID int NOT NULL AUTO_INCREMENT,
-    GenreID int NOT NULL AUTO_INCREMENT,
-    FOREIGN KEY (MovieID) REFERENCES movie(MovieID)
+    movieID int NOT NULL,
+    genreID int NOT NULL,
+    FOREIGN KEY (movieID) REFERENCES movie(ID)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-    FOREIGN KEY (GenreID) ReleaseDate Genre(GenreID)
+    FOREIGN KEY (genreID) REFERENCES Genre(ID)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
 CREATE TABLE reviews(
     reviewID int NOT NULL AUTO_INCREMENT,
+    movieID int NOT NULL,
     reviewSentence text NOT NULL,
     name varchar(70) NOT NULL,
     rating int NOT NULL,
     PRIMARY KEY(reviewID)
-    FOREIGN KEY(MovieID) REFERENCES movie(MovieID)
+    FOREIGN KEY(movieID) REFERENCES movie(ID)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
 CREATE TABLE users(
-    userID int NOT NULL AUTO_INCREMENT,
-    username varchar(100) NOT NULL,
-    name varchar(70) NOT NULL,
+    ID int NOT NULL AUTO_INCREMENT,
+    username varchar(50) NOT NULL,
     password char(60) NOT NULL
     role varchar(50) NOT NULL,
+    lastloginip varchar(15) NULL,
+    lastlogintime datetime NULL,
     UNIQUE (username),
     PRIMARY KEY(userID)
 );
 
-CREATE TABLE GenreID(
-    GenreID int NOT NULL AUTO_INCREMENT,
-    GenreName varchar(150) NOT NULL,
-    GenreDesc text NOT NULL,
-    UNIQUE (GenreName),
-    PRIMARY KEY(GenreID)
+
+CREATE TABLE MovienMod(
+	timelog datetime null,
+	userID int NOT NULL,
+	movieID int NOT NULL,
+	FOREIGN KEY (movieID) REFERENCES movie(ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (userID) REFERENCES users(ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE MovieCreate(
+	timelog datetime null,
+	userID int NOT NULL,
+	movieID int NOT NULL,
+	FOREIGN KEY (movieID) REFERENCES movie(ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (userID) REFERENCES users(ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
