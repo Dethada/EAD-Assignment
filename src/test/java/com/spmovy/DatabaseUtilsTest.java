9fa742ca76c45039c21d7168f714cc166f2ca4e7
PotatoDrug <zhy.david@gmail.com>
2018-05-05 00:36:41 +0800

    will it work now

---
+++ src/test/java/com/spmovy/DatabaseUtilsTest.java
@@ -0,0 +1,41 @@
+package com.spmovy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+
+import static org.junit.Assert.*;
+
+public class DatabaseUtilsTest {
+    DatabaseUtils db;
+    int id;
+    @Before
+    public void setUp() throws Exception {
+        this.db = new DatabaseUtils();
+    }
+
+    @Test
+    public void executeQuery() throws Exception {
+        ResultSet rs = this.db.executeQuery("SELECT * FROM Genre WHERE name=?", "Action");
+        assertTrue(rs.next());
+    }
+
+    @Test
+    public void executeUpdate() throws Exception {
+        assertEquals(this.db.executeUpdate("INSERT INTO Genre(name, description) VALUES(?,?)", "unittesting", "unittesting"), 1);
+        assertEquals(this.db.executeUpdate("DELETE FROM Genre WHERE name=? AND description=?", "unittesting", "unittesting"), 1);
+    }
+
+    @Test
+    public void executeFixedQuery()throws Exception {
+        ResultSet rs = this.db.executeFixedQuery("SELECT ID FROM Genre where name=\'Action\'");
+        assertTrue(rs.next());
+        this.id = rs.getInt("ID");
+    }
+
+    @Test
+    public void getConnection() throws Exception {
+        assertNotNull(this.db.getConnection());
+    }
+}
