3bac7e2d18ca820f8bfb7047ab4198836d683453
PotatoDrug <zhy.david@gmail.com>
2018-07-23 21:38:03 +0800

    converted user to mvc

--- src/main/java/com/spmovy/servlet/ChangePassword.java
+++ src/main/java/com/spmovy/servlet/ChangePassword.java
@@ -1,19 +1,15 @@
 package com.spmovy.servlet;
 
-import com.spmovy.DatabaseUtils;
-import com.spmovy.Utils;
+import com.spmovy.beans.UserJB;
+import com.spmovy.beans.UserJBDB;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.sql.ResultSet;
 import java.io.IOException;
 import java.sql.SQLException;
-
-import static com.spmovy.BCryptUtil.hashPassword;
-import static com.spmovy.BCryptUtil.checkPassword;
 
 @WebServlet("/backend/admin/ChangePassword")
 public class ChangePassword extends HttpServlet {
@@ -22,46 +18,28 @@ public class ChangePassword extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String currentpass = request.getParameter("currentpass");
         String newpass = request.getParameter("newpass");
-        if (currentpass == null || newpass == null) {
-            response.sendRedirect("/admin/changePassword.jsp?result=failed");
-            return;
-        }
-        DatabaseUtils db = Utils.getDatabaseUtils(response);
-        if (db == null) return; // return if database connection failed
         HttpSession session = request.getSession();
-        int userid;
-        try {
-            userid = (int) session.getAttribute("userid");
-        } catch(NumberFormatException | IllegalStateException | NullPointerException e) {
+        if (session == null) {
             response.sendRedirect("/errors/error.html");
+            System.out.println("1");
             return;
         }
+        UserJB user = (UserJB) session.getAttribute("user");
+        if (user == null) {
+            response.sendRedirect("/errors/error.html");
+            System.out.println("2");
+            return;
+        }
+        int userid = user.getID();
         try {
-            ResultSet rs = db.executeQuery("select password from users where ID=?", userid);
-            if (rs.next()) {
-                if (checkPassword(currentpass, rs.getString("password"))) {
-                    // change password here
-                    int updateCount = db.executeUpdate("update users set password=? where ID=?", hashPassword(newpass), userid);
-                    if (updateCount != 1) {
-                        response.sendRedirect("/errors/error.html");
-                        return;
-                    } else {
-                        response.sendRedirect("/admin/changePassword.jsp?result=success");
-                        return;
-                    }
-                } else {
-                    // current password not correct, abort
-                    response.sendRedirect("/admin/changePassword.jsp?result=failed");
-                    return;
-                }
+            if(UserJBDB.changePassword(userid, currentpass, newpass)) {
+                response.sendRedirect("/admin/changePassword.jsp?result=success");
             } else {
                 response.sendRedirect("/admin/changePassword.jsp?result=failed");
-                return;
             }
         } catch (SQLException e) {
             e.printStackTrace();
             response.sendRedirect("/errors/error.html");
-            return;
         }
     }
 }
--- src/test/java/com/spmovy/servlet/ChangePasswordTest.java
+++ src/test/java/com/spmovy/servlet/ChangePasswordTest.java
@@ -1,5 +1,6 @@
 package com.spmovy.servlet;
 
+import com.spmovy.beans.UserJB;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
@@ -29,42 +30,5 @@ public class ChangePasswordTest extends Mockito {
         verify(request, atLeast(1)).getParameter("newpass");
         verify(response).sendRedirect(captor.capture());
         assertEquals("/errors/error.html", captor.getValue());
-    }
-
-    @Test
-    public void invalidUserID() throws Exception {
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        HttpSession session = mock(HttpSession.class);
-        session.setAttribute("userid", "abc");
-
-        when(request.getParameter("currentpass")).thenReturn("abc");
-        when(request.getParameter("newpass")).thenReturn("abc");
-        when(request.getSession()).thenReturn(session);
-
-        new ChangePassword().doPost(request, response);
-
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(request, atLeast(1)).getParameter("currentpass");
-        verify(request, atLeast(1)).getParameter("newpass");
-        verify(response).sendRedirect(captor.capture());
-        assertEquals("/errors/error.html", captor.getValue());
-    }
-
-    @Test
-    public void nullParams() throws Exception {
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-
-        when(request.getParameter("currentpass")).thenReturn(null);
-        when(request.getParameter("newpass")).thenReturn(null);
-
-        new ChangePassword().doPost(request, response);
-
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(request, atLeast(1)).getParameter("currentpass");
-        verify(request, atLeast(1)).getParameter("newpass");
-        verify(response).sendRedirect(captor.capture());
-        assertEquals("/admin/changePassword.jsp?result=failed", captor.getValue());
     }
 }
